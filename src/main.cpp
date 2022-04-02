708b15321e02c4252a8c7e3b967ad85890604fe8
baerlux <baerlux@pm.me>
2022-04-02 23:59:39 +0200

    add player and movement ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,62 @@
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+
 #include <docopt/docopt.h>
 #include <spdlog/spdlog.h>
 
 #include <internal_use_only/config.hpp>
 
 
-void run_game() { fmt::print("hi!\n"); }
+struct Pos
+{
+  int x, y;
+};
+
+struct Player
+{
+  Pos pos;
+};
+
+void run_game()
+{
+  Player player{};
+
+  auto upButton = ftxui::Button("N ᐃ", [&] { player.pos.y++; });
+  auto leftButton = ftxui::Button("W ᐊ", [&] { player.pos.x--; });
+  auto downButton = ftxui::Button("S ᐁ", [&] { player.pos.y--; });
+  auto rightButton = ftxui::Button("E ᐅ", [&] { player.pos.x++; });
+
+  auto moveCtl = ftxui::Container::Vertical({
+    ftxui::Renderer(upButton,
+      [&] {
+        return ftxui::hbox({
+          ftxui::filler(),
+          upButton->Render(),
+          ftxui::filler(),
+        });
+      }),
+    ftxui::Container::Horizontal({
+      leftButton,
+      downButton,
+      rightButton,
+    }),
+  });
+
+  auto ui = ftxui::Renderer(moveCtl, [&] {
+    return ftxui::window(ftxui::text("smoothlife"),
+      ftxui::vbox({
+        ftxui::text(fmt::format("{}, {}", player.pos.x, player.pos.y)),
+        ftxui::separator(),
+        ftxui::hbox({
+          moveCtl->Render(),
+        }),
+      }));
+  });
+
+  auto screen = ftxui::ScreenInteractive::FitComponent();
+  screen.Loop(ui);
+}
 
 
 int main(int argc, const char **argv)
