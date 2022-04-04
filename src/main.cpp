2adc08ae4e792a4f2bb3baeba50f1860a532b942
baerlux <baerlux@pm.me>
2022-04-04 05:26:05 +0200

    add gameboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
@@ -8,23 +10,18 @@
 #include <internal_use_only/config.hpp>
 
 
-struct Pos
-{
-  int x, y;
-};
-
 struct Player
 {
-  Pos pos{};
+  int x, y;
   ftxui::Components buttons;
   ftxui::Component move_ui;
 
-  Player()
+  Player(int x_, int y_) : x{ x_ }, y{ y_ }
   {
-    buttons.push_back(ftxui::Button("N ᐃ", [&] { pos.y++; }));
-    buttons.push_back(ftxui::Button("W ᐊ", [&] { pos.x--; }));
-    buttons.push_back(ftxui::Button("S ᐁ", [&] { pos.y--; }));
-    buttons.push_back(ftxui::Button("E ᐅ", [&] { pos.x++; }));
+    buttons.push_back(ftxui::Button("N ᐃ", [&] { y++; }));
+    buttons.push_back(ftxui::Button("W ᐊ", [&] { x--; }));
+    buttons.push_back(ftxui::Button("S ᐁ", [&] { y--; }));
+    buttons.push_back(ftxui::Button("E ᐅ", [&] { x++; }));
 
     move_ui = ftxui::Container::Vertical({
       ftxui::Renderer(buttons[0],
@@ -66,14 +63,56 @@ struct Player
   }
 };
 
+template<std::size_t Width, std::size_t Height> struct GameBoard
+{
+  explicit GameBoard(Player &p) : player{ p } {}
+
+  [[nodiscard]] int get_field(std::size_t x, std::size_t y) const { return state.at(x + Width * y); }
+
+  void set_field(std::size_t x, std::size_t y, int value)
+  {
+    assert(value >= 0 && value < 10);// NOLINT magic number okay here
+    state.at(x + Width * y) = value;
+  }
+
+  [[nodiscard]] ftxui::Element render() const
+  {
+    std::vector<ftxui::Element> rows;
+
+    for (std::size_t y = Height - 1; y < Height; --y) {
+      std::vector<ftxui::Element> row;
+      for (std::size_t x = 0; x < Width; ++x) {
+        std::string field = " ";
+        int value = get_field(x, y);
+        if (value > 0) { field = std::to_string(value); }
+        if (static_cast<int>(x) == player.x && static_cast<int>(y) == player.y) { field = "@"; }
+        row.push_back(ftxui::text(fmt::format(" {} ", field)) | ftxui::border);
+      }
+      rows.push_back(ftxui::hbox(std::move(row)));
+    }
+
+    return ftxui::vbox(std::move(rows));
+  }
+
+private:
+  std::array<int, Width * Height> state{};
+  Player &player;
+};
+
 void run_game()
 {
-  Player player;
+  Player player{ 2, 2 };
+  GameBoard<5, 5> board{ player };// NOLINT magic number okay
+  board.set_field(0, 3, 6);// NOLINT magic number
+  board.set_field(4, 1, 7);// NOLINT magic number
+  board.set_field(1, 2, 2);// NOLINT magic number
 
   auto game_ui = ftxui::Renderer(player.move_ui, [&] {
     return ftxui::window(ftxui::text(" smoothlife "),
       ftxui::vbox({
-        ftxui::text(fmt::format("{}, {}", player.pos.x, player.pos.y)),
+        ftxui::text(fmt::format("{}, {}", player.x, player.y)),
+        ftxui::separator(),
+        board.render(),
         ftxui::separator(),
         ftxui::hbox({
           player.move_ui->Render(),
@@ -94,6 +133,7 @@ int main(int argc, const char **argv)
     Usage:
           smoothlife
           smoothlife test spdlog
+          smoothlife test numbers
           smoothlife --version
           smoothlife (-h | --help)
     Options:
