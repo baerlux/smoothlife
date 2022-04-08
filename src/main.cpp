6841c9de947926c4ccbcbf4bc41b135958b9be6d
baerlux <baerlux@pm.me>
2022-04-08 12:46:13 +0200

    gameplay prototype 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <array>
+#include <random>
+#include <vector>
 
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
@@ -12,19 +14,20 @@
 
 struct Player
 {
-  // position
-  int x, y;
-  int energy = 4;
+  // set by GameBoard
+  int x{}, y{};
+  int energy{};
+  int surface{};
 
-  // ui
+  std::function<void()> interaction;
+  ftxui::Box bounds;
+
+  // player control ui
   ftxui::Components buttons;
   ftxui::Component move_ui;
 
-  // defined by GameBoard
-  ftxui::Box bounds;
-  std::function<void()> interaction;
 
-  Player(int x_, int y_) : x{ x_ }, y{ y_ }
+  Player()
   {
     buttons.push_back(ftxui::Button(" ᐃ ", [&] {
       if (y < bounds.y_max && energy > 0) {
@@ -102,61 +105,156 @@ struct Field
   Type type = Type::empty;
   int value = 0;
 
-  void apply(Player &player)
+  using enum Type;
+
+  [[nodiscard]] Field inverse() const
   {
-    using enum Field::Type;
     switch (type) {
     case add:
-      player.energy += value;
+      return { sub, value };
+    case sub:
+      return { add, value };
+    case mul:
+      return { div, value };
+    case div:
+      return { mul, value };
+    default:
+      return { empty, value };
+    }
+  }
+
+  void apply(int &surface)
+  {
+    switch (type) {
+    case add:
+      surface += value;
       break;
     case sub:
-      player.energy -= value;
+      surface -= value;
       break;
     case mul:
-      player.energy *= value;
+      surface *= value;
       break;
     case div:
-      player.energy /= value;
+      surface /= value;
       break;
-    case exit:
-      return;
     default:
-      break;
+      return;
     }
-    // reset field
+    // reset field if it was applied
     type = empty;
     value = 0;
   }
 };
 
-template<std::size_t Width, std::size_t Height> struct GameBoard
-{
-  std::array<Field, Width * Height> state{};
-  Player &player;
 
-  explicit GameBoard(Player &p) : player{ p }
+int roundness(int num)
+{
+  int r = 0;
+  int d = 10;// NOLINT
+  while (true) {
+    if (num % d == 0) {
+      ++r;
+    } else {
+      break;
+    }
+    d *= 10;// NOLINT
+  }
+  return r;
+}
+
+
+template<std::size_t Width, std::size_t Height, class Prng> struct GameBoard
+{
+  std::array<Field, Width * Height> state;
+  Player &player;
+  Prng &rnd;
+
+  using enum Field::Type;
+  explicit GameBoard(Prng &randomness_provider, Player &p) : player{ p }, rnd{ randomness_provider }
   {
     player.bounds.x_max = Width - 1;
     player.bounds.x_min = 0;
     player.bounds.y_max = Height - 1;
     player.bounds.y_min = 0;
-    player.interaction = [&] { get_field(player.x, player.y).apply(player); };
+    player.interaction = [&] {
+      Field &f = get_field(player.x, player.y);
+      switch (f.type) {
+      case empty:
+        break;
+      case exit:
+        generate_level();
+        break;
+      default:
+        f.apply(player.surface);
+        break;
+      }
+    };
   }
 
-  [[nodiscard]] const Field &get_field(int x, int y) const
+  void generate_level()
   {
-    return state.at(static_cast<std::size_t>(x) + Width * static_cast<std::size_t>(y));
+    // clear field
+    state = std::array<Field, Width * Height>{};
+
+    using uni = std::uniform_int_distribution<int>;
+    uni in_board_width{ 0, Width - 1 };
+    uni in_board_height{ 0, Height - 1 };
+    uni in_field_types{ 2, 5 };// NOLINT
+    uni in_one_to_nine{ 1, 9 };// NOLINT
+
+    int goal = 10000;// in_one_to_nine(rnd) * 1000;// NOLINT
+
+    std::array<bool, Width * Height> occupied{};
+
+    // player
+    occupied.at(pack2d(0, 0)) = true;
+    player.energy = 100;// NOLINT
+    player.x = 0;
+    player.y = 0;
+
+    // exit
+    occupied.at(pack2d(Width - 1, Height - 1)) = true;
+    set_field(Width - 1, Height - 1, { exit });
+
+    // generate field chain
+    for (int i = 0; i < 5; ++i) {// NOLINT
+
+      Field rnd_f{ static_cast<Field::Type>(in_field_types(rnd)), in_one_to_nine(rnd) };
+
+      if (rnd_f.type == mul) {
+        int rem = goal % rnd_f.value;
+        while (rem != 0) {
+          rnd_f.value = in_one_to_nine(rnd);
+          rem = goal % rnd_f.value;
+        }
+      }
+
+      int rnd_x{};
+      int rnd_y{};
+
+      // search for empty field
+      do {
+        rnd_x = in_board_width(rnd);
+        rnd_y = in_board_height(rnd);
+      } while (occupied.at(pack2d(rnd_x, rnd_y)));
+
+      set_field(rnd_x, rnd_y, rnd_f);// NOLINT
+      occupied.at(pack2d(rnd_x, rnd_y)) = true;
+
+      rnd_f.inverse().apply(goal);
+    }
+
+    player.surface = goal;
   }
 
-  [[nodiscard]] Field &get_field(int x, int y)
-  {
-    return state.at(static_cast<std::size_t>(x) + Width * static_cast<std::size_t>(y));
-  }
+  static size_t pack2d(int x, int y) { return static_cast<std::size_t>(x) + Width * static_cast<std::size_t>(y); }
 
-  void set_field(int x, int y, Field field)
-  {
-    state.at(static_cast<std::size_t>(x) + Width * static_cast<std::size_t>(y)) = field;
-  }
+  [[nodiscard]] const Field &get_field(int x, int y) const { return state.at(pack2d(x, y)); }
+
+  [[nodiscard]] Field &get_field(int x, int y) { return state.at(pack2d(x, y)); }
+
+  void set_field(int x, int y, Field field) { state.at(pack2d(x, y)) = field; }
 
   [[nodiscard]] ftxui::Element render() const
   {
@@ -165,16 +263,34 @@ template<std::size_t Width, std::size_t Height> struct GameBoard
     for (int y = Height - 1; y >= 0; --y) {
       std::vector<ftxui::Element> row;
       for (int x = 0; x < static_cast<int>(Width); ++x) {
-        std::string repr = " ";
-        const Field &field = get_field(x, y);
         using enum Field::Type;
-        if (field.type != empty && field.type != exit && field.value > 0 && field.value < 10) {// NOLINT
+        using enum ftxui::Color::Palette16;
+        std::string repr = " ";
+        ftxui::Color color = White;
+        const Field &field = get_field(x, y);
+        if (field.type != empty && field.type != exit) {
           repr = std::to_string(field.value);
+          switch (field.type) {
+          case add:
+            color = RedLight;
+            break;
+          case sub:
+            color = BlueLight;
+            break;
+          case mul:
+            color = GreenLight;
+            break;
+          case div:
+            color = YellowLight;
+            break;
+          default:
+            break;
+          }
         } else if (field.type == exit) {
-          repr = "⛝";
+          repr = "⋒";
         }
-        if (x == player.x && y == player.y) { repr = "@"; }
-        row.push_back(ftxui::text(fmt::format(" {} ", repr)) | ftxui::border);
+        if (x == player.x && y == player.y) { repr = "🯅"; }
+        row.push_back(ftxui::text(fmt::format(" {} ", repr)) | ftxui::border | ftxui::color(color));
       }
       rows.push_back(ftxui::hbox(std::move(row)));
     }
@@ -185,24 +301,26 @@ template<std::size_t Width, std::size_t Height> struct GameBoard
 
 void run_game()
 {
-  Player player{ 2, 2 };
-  GameBoard<5, 5> board{ player };// NOLINT magic number okay
-
-  using enum Field::Type;
-  board.set_field(0, 3, { div, 2 });// NOLINT magic number
-  board.set_field(4, 1, { mul, 7 });// NOLINT magic number
-  board.set_field(1, 2, { add, 4 });// NOLINT magic number
-  board.set_field(4, 0, { exit });// NOLINT magic number
+  Player player;
+  auto prng = std::ranlux24{ std::random_device{}() };
+  GameBoard<7, 5, std::ranlux24> board{ prng, player };// NOLINT magic number okay
+  board.generate_level();
 
   auto game_ui = ftxui::Renderer(player.move_ui, [&] {
     return ftxui::window(ftxui::text(" smoothlife "),
-      ftxui::vbox({
-        ftxui::text(fmt::format("energy: {}", player.energy)),
+      ftxui::hbox({
+        ftxui::vbox({
+          ftxui::filler(),
+          player.move_ui->Render(),
+          ftxui::filler(),
+        }),
         ftxui::separator(),
         board.render(),
         ftxui::separator(),
-        ftxui::hbox({
-          player.move_ui->Render(),
+        ftxui::vbox({
+          ftxui::text(""),
+          ftxui::text(fmt::format("  energy  : {}    ", player.energy)),
+          ftxui::text(fmt::format("  surface : {}    ", player.surface)),
         }),
       }));
   });
